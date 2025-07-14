f86691e767119fff1d0d22d1b2aa115df395bea5
harshrajk <harshu.1994k@gmail.com>
2025-07-15 00:48:29 +0530

    app level maintenance added

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -4,10 +4,18 @@ import NewList from "./pages/NewList";
 import Maintenance from "./pages/Maintenance";
 
 export default function App() {
+  console.log(import.meta.env.VITE_IS_MAINTENANCE_MODE === "true");
+  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {
+          // If maintenance mode is enabled, redirect to maintenance page
+          import.meta.env.VITE_IS_MAINTENANCE_MODE === "true" ? (
+            <Route path="/" element={<Maintenance />} />
+          ) : <Route path="/" element={<Home />} />
+        }
+        
         <Route path="/maintenance" element={<Maintenance />} />
         <Route path="/new" element={<NewList />} />
       </Routes>
