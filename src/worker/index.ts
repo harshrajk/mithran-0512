cecbc004425e3d47faaa949de264c21cd8e66b01
harshrajk <harshu.1994k@gmail.com>
2025-07-15 20:53:19 +0530

    failsafe

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -35,17 +35,17 @@ app.post("/api/new", async (c) => {
             } catch (error) {
                 console.error("Error uploading file to R2:", error);
             }
-            item.imageUrl = `https://assets.decalist.xyz/${key}`; // Replace with your actual R2 public URL
+            item.image_url = `https://assets.decalist.xyz/${key}`; // Replace with your actual R2 public URL
         } else {
             console.log(`No file uploaded for item ${index + 1}, using provided imageUrl: ${item.image_url}`);
         }
 
         finalItems.push({
-            title: item.title,
-            description: item.description,
-            position : item.position,
-            image_url: item.imageUrl || item.image_url, // Use the uploaded image URL or the provided one
-            external_url: item.externalUrl
+            title: item.title ?? "",
+            description: item.description ?? "",
+            position : item.position ?? 0,
+            image_url: item.image_url ?? "", // Use the uploaded image URL or the provided one
+            external_url: item.externalUrl ?? "",
         });
         
     });
